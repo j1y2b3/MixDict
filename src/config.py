6ca86f10ad22a383b6e07a3d49c2048e9aa295a1
j1y2b3 <2162539907@qq.com>
2026-08-21 13:29:31 +0800

    feat: 添加全局设置

---
+++ src/config.py
@@ -0,0 +1,14 @@
+"""Global settings."""
+
+TITLE = "简易网络词典"
+WINDOW_SIZE_RATE = (0.6, 0.7)  # width, height
+WINDOW_MIN_SIZE = (640, 480)  # width, height
+
+DEFAULT_TIMEOUT = 10.0
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/151.0.0.0 "
+    "Safari/537.36"
+)
+DEFAULT_DICTIONARY_SOURCE = "Youdao"
