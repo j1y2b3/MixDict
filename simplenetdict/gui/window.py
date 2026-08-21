e3eb6c8d7a958c6649d3ec3d015cd6fc662351d5
j1y2b3 <2162539907@qq.com>
2026-08-21 21:00:03 +0800

    refactor(api): 重构当前源设置方法，移除冗余显示名称储存

--- simplenetdict/gui/window.py
+++ simplenetdict/gui/window.py
@@ -22,21 +22,18 @@ class DictApi:
         self.sources_registry = sources_registry
         self.source_reg_name = self.sources_registry.default_source_reg_name
         self.source: DictionarySource
-        self.source_name: str
-        self.change_source(self.source_reg_name)
+        self.set_source(self.source_reg_name)
 
     def list_sources(self) -> list[str]:
         """Return the `reg_name`s of all registered sources."""
         return self.sources_registry.list()
 
-    def change_source(self, reg_name: str):
-        """Switch the current source to `reg_name`."""
-
-        source, name = self.sources_registry.get(reg_name)
-        if source is None or name is None:
+    def set_source(self, reg_name: str):
+        """Set the current source to `reg_name`."""
+        source = self.sources_registry.get(reg_name)
+        if source is None:
             raise ValueError(f"Unknown source: {reg_name!r}")
         self.source = source
-        self.source_name = name
 
     def lookup(self, word: str) -> dict:
         """Use current source looking up `word`, return data format according to simplenetdict/schema.py."""
@@ -54,15 +51,15 @@ def run(sources_registry: SourcesRegistry, debug: bool = False):
 
     webview.create_window(
         title=config.TITLE,
-        url=str(resources.web_path("index.html")),  # pywebview will start a built-in HTTP server automatically
+        url=str(resources.web_path("index.html")),  # pywebview will start a built-in HTTP server automatically.
         js_api=DictApi(sources_registry),
         width=width,
         height=height,
         min_size=config.WINDOW_MIN_SIZE,
-        screen=screen,  # pywebview automatically centers the window
+        screen=screen,  # pywebview automatically centers the window.
         text_select=True
     )
-    if flags.dev_mode:  # Start with `-X dev`
+    if flags.dev_mode:  # Start with `-X dev`.
         debug = True
     webview.start(debug=debug)
 
