0d5d46162f1a30fd4cb003c2889cc17bfb82bfe5
j1y2b3 <2162539907@qq.com>
2026-09-13 18:09:52 +0800

    refactor(sources): 打包后用户词典源路径由与可执行文件同目录改为用户文件夹下

--- mixdict/resources.py
+++ mixdict/resources.py
@@ -52,7 +52,7 @@ def user_sources_dir() -> Path:
     """
     if getattr(sys, "frozen", False):
         # Packed: writable, in the same directory as the executable file
-        path = Path(sys.executable).parent / "user_sources"
+        path = platformdirs.user_data_path() / "user_sources"
     else:
         path = ROOT_DIR / "user_sources"  # Not packed: user_sources/ in the project root
 
